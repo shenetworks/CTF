8bc9ed064dd02930cfeb2fcfadc183c5612a2fe9
shenetworks <91444360+shenetworks@users.noreply.github.com>
2022-10-06 12:52:15 -0500

    Update corridor.py

--- corridor.py
+++ corridor.py
@@ -5,7 +5,7 @@ hashes = []
 
 for x in num:
     hashes.append(hashlib.md5(str(x).encode('utf-8')).hexdigest())
-with open('hashdump.txt', 'w') as fname"
+with open('hashdump.txt', 'w') as fname:
     for i in hashes:
         fname.write(i + '\n')
 print('hashing completed')
