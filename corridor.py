2149293a488ca0bbe504c6c2382e3a286e3f60f8
shenetworks <91444360+shenetworks@users.noreply.github.com>
2022-10-06 10:59:42 -0500

    Add files via upload

---
+++ corridor.py
@@ -0,0 +1,11 @@
+import hashlib
+
+num = range(101)
+hashes = []
+
+for x in num:
+    hashes.append(hashlib.md5(str(x).encode('utf-8')).hexdigest())
+with open('hashdump.txt', 'w') as fname"
+    for i in hashes:
+        fname.write(i + '\n')
+print('hashing completed')
